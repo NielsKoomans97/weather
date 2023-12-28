1bf2f95a9c7b30bc48b8b4cc003c706033a0abc6
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2023-12-28 02:38:29 +0100

    A whole lot of restructuring, and refactoring

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <script src="js/main.js" type="module" defer></script>
+        <script src="https://kit.fontawesome.com/9efe11bfbd.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="scss/style.css">
+    </head>
+<body class="page">
+    <?php   require_once './html/radar.html';
+            require_once './html/tempanimation.html';
+            require_once './html/warnings.html' ?>
+</body>
+</html>
